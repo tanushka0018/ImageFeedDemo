b8f2c4ce873999c002d408b7804707ddf733324d
Tatiana <tanushka0018@gmail.com>
2026-03-16 20:42:39 -0700

    fix all issues based on the "Авторское решение"

---
+++ ImageFeedDemo/ImagesList/ImagesListCell.swift
@@ -0,0 +1,12 @@
+//
+//  ImagesListCell.swift
+//  ImageFeedDemo
+//
+//  Created by Tatiana on 2/12/26.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+}
